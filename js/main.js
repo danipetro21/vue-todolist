212c1a2c0910a19ea855a90d05d73de6e842a3ea
danipetro21 <idany346@gmail.com>
2022-11-30 10:08:27 +0100

    esercizio base

--- js/main.js
+++ js/main.js
@@ -4,10 +4,20 @@ createApp({
     data() {
         return {
             //inserimento tasks all'interno dell'applicazione
+            error: false,
             tasks: [
-                { text: 'Fare i compiti', done: false },
-                { text: 'Fare la spesa', done: true },
-                { text: 'Fare il bucato', done: false }
+                {
+                    text: 'Fare i compiti',
+                    done: false
+                },
+                {
+                    text: 'Fare la spesa',
+                    done: true
+                },
+                {
+                    text: 'Fare il bucato',
+                    done: false
+                }
             ],
             myInput: ''
 
@@ -16,13 +26,17 @@ createApp({
     methods: {
         addTask() {
             if (this.myInput === "") {
-                return;
+                error = true;
+                
             } else {
-                this.tasks.push({ text: this.myInput });
+                this.tasks.push({ text: this.myInput, done: false });
                 console.log(this.myInput);
             }
             //azzera l'input type text
             this.myInput = "";
+        },
+        removeTask(index) {
+            this.tasks.splice(index, 1);
         }
     }
 }).mount("#app");
